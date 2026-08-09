2cccc8a1b68498ff3f4208d1fbdaeceac8376bdd
danish almer <shaikhkingdanish@gmail.com>
2026-08-09 13:35:52 +0530

    Update and rename README.md to numpy.py

---
+++ numpy.py
@@ -0,0 +1,24 @@
+               
+next project coming soon                   
+import pandas as pd
+
+data = {
+    "Name": ["Danish", "Rahul", "Danish", "Aman"],
+    "Age": [18, 19, 18, 20]
+}
+
+df = pd.DataFrame(data)
+
+print("Before removing duplicates:")
+print(df)
+
+df.drop_duplicates(inplace=True)
+
+print("\nAfter removing duplicates:")
+print(df)   
+                
+  
+                                       
+                                                    
+                                   
+
