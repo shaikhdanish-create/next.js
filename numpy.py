c21bfa4f2d5c54e4ca8e2ab6f1f174775870f630
danish almer <shaikhkingdanish@gmail.com>
2026-08-10 17:39:42 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -16,7 +16,15 @@ df.drop_duplicates(inplace=True)
 
 print("\nAfter removing duplicates:")
 print(df)   
-                  
+
+
+# Create a 5x5 matrix with values 1,2,3,4 just below the diagonal
+
+import numpy as np
+
+arr = np.diag([1, 2, 3, 4], k=-1)
+
+print(arr)
   
                                        
                                                     
